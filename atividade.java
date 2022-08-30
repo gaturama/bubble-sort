d5a3c48e2af748098a538965d48c2e3a46c136a5
gaturama <bielsobv@gmail.com>
2022-08-30 20:20:25 -0300

    atividade avaliativa 1

---
+++ atividade.java
@@ -0,0 +1,32 @@
+public class atividade {
+
+    public static void main (String[] args) {
+        System.out.println("*---------------------*");
+        System.out.println(" ATIVIDADE AVALIATIVA ");
+        System.out.println("*---------------------*");
+
+        int num = 0;
+
+        switch(num){
+
+            case 1:
+
+            System.out.println(" ");
+
+            break;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
