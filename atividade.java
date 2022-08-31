e6eebb9e5bc07ea2719dcc2c13baea478755f8ee
gaturama <bielsobv@gmail.com>
2022-08-30 21:42:27 -0300

    adição de comandos na atividade avaliativa

--- atividade.java
+++ atividade.java
@@ -1,3 +1,11 @@
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.File;
+
 public class atividade {
 
     public static void main (String[] args) {
@@ -6,7 +14,21 @@ public class atividade {
         System.out.println(" ATIVIDADE AVALIATIVA ");
         System.out.println("*---------------------*");
 
-        int[] vetor = new int [5]; 
+        String valores = new String();
+        String arqv = "C:/Users/Gabriel/github/yourrepositories/bubblesort/arquivo.txt";
+
+        File scan = new File (arqv);
+
+        int[] vetor = new int [5];
+        
+        while(scan.exists()){
+            try{
+                FileReader leitura = new FileReader(arqv);
+                    BufferedReader bufferedReader = new BufferedReader(leitura);
+            }catch(IOException e){
+                
+            }
+        }
 
             //ABAIXO ESTÃO OS VALORES RECEBIDOS DO ARQUIVO DE TEXTO.   
             vetor[0] = 5;
