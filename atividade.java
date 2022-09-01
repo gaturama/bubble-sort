98a47c600bf5e95072b2d9f5023687fe17363a01
gaturama <bielsobv@gmail.com>
2022-08-31 22:29:07 -0300

    adição de comandos na atividade avaliativa

--- atividade.java
+++ atividade.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.File;
 import java.util.Arrays;
 
+
 public class atividade { 
 
     public static void main (String[] args) {
@@ -23,36 +24,50 @@ public class atividade {
         int[] vetor = new int [10];
         
         
-            try{
-                // Puxando os dados do arquivo de texto para o programa
-                FileReader leitura = new FileReader(arqv);
-                    BufferedReader bufferedReader = new BufferedReader(leitura); //função para ler o arquivo
-                        int linhas = 0;
-                            while(bufferedReader.readLine()!= null){
-                                linhas++;
-                            }
-                            bufferedReader.close(); //fechamento da leitura
+        try{
+            // Puxando os dados do arquivo de texto para o programa
+            FileReader leitura = new FileReader(arqv);
+            BufferedReader bufferedReader = new BufferedReader(leitura); //função para ler o arquivo
+            int linhas = 0;
+            while(bufferedReader.readLine()!= null){
+                linhas++;
+            }
+            bufferedReader.close(); //fechamento da leitura
 
-                                int[] lista = new int[linhas]; // programa irá ler linha a linha do arquivo
-                                    leitura = new FileReader(arqv);
-                                        bufferedReader = new BufferedReader(leitura); // releitura do arquivo
-                                            linhas = 0;
-                                                String linha ="";
-                                                     while((linha = bufferedReader.readLine())!= null){
-                                                        lista[linhas] = Integer.parseInt(linha);
-                                                             linhas++;
-                                                        }
-                                                             bufferedReader.close(); // outro fechamento de leitura
+            int[] lista = new int[linhas]; // programa irá ler linha a linha do arquivo
+            leitura = new FileReader(arqv);
+            bufferedReader = new BufferedReader(leitura); // releitura do arquivo
+            linhas = 0;
+            String linha ="";
+            while((linha = bufferedReader.readLine())!= null){
+                lista[linhas] = Integer.parseInt(linha);
+                linhas++;
+            }
+            bufferedReader.close(); // outro fechamento de leitura
 
-                                                                System.out.println(Arrays.toString(lista));
+            System.out.println("Array desordenado: ");
+            System.out.println(Arrays.toString(lista));
 
-                                                                    for (int i = linhas; i < 0; i++ ){ //tentativa de ordenar o array :(
-                                                                         System.out.println(vetor[linhas]);
-                                                                     }
-                                                                 }catch(IOException e){
-                                                            System.out.println("Deu erro, tente novamente!" + e.getMessage()); // mensagem de erro, caso o usuário faça algo errôneo
-                                                             } 
-                                                         }
-                                                      }
+            System.out.println("Passos da Ordenação: ");
+
+            int n = lista.length;
+            for (int i = 0; i < n - 1; i++)
+                for (int j = 0; j < n - i - 1; j++)
+                    if (lista[j] > lista[j + 1]) {
+                        // swap lista[j+1] and lista[j]
+                        int temp = lista[j];
+                        lista[j] = lista[j + 1];
+                        lista[j + 1] = temp;
+
+                        System.out.println(Arrays.toString(lista));
+                    }
+            
+            System.out.println("Array ordenado: ");
+            System.out.println(Arrays.toString(lista));
+        } catch(IOException e){
+            System.out.println("Deu erro, tente novamente!" + e.getMessage()); // mensagem de erro, caso o usuário faça algo errôneo
+        } 
+    }
+}
                           
 
